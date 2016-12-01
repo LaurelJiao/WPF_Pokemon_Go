d61ab14590487ce12f35f1c06f7680d89068ddd5
potterxu <xujingchuan1995@gmail.com>
2016-12-02 05:59:13 +0800

    draft of page switch, battle_gym, part of pokemon and presenter

---
+++ Pokemon_Go/Pokemon_Go/Presenter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pokemon_Go
+{
+    class GUI_Presenter
+    {
+        private readonly IView View;
+        public GUI_Presenter (IView View)
+        {
+            this.View = View;
+        }
+    }
+}
